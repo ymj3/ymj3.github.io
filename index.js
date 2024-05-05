e1e1e47360eddafa79414c97989b714419863b28
ymj3 <3602616802@qq.com>
2024-05-06 00:08:27 +0800

    upd

--- index.js
+++ index.js
@@ -5,18 +5,15 @@ const body = document.querySelector('body'),
     modeSwitch = body.querySelector(".toggle-switch"),
     modeText = body.querySelector(".mode-text");
 
-var mainbody = document.getElementById("mainbody");
 // 点击toggle元素时触发事件
 toggle.addEventListener("click", () => {
     // 切换shell元素的close类
     shell.classList.toggle("close");
-    mainbody.style.left = 98 + "px";
 })
 // 点击searchBtn元素时触发事件
 searchBtn.addEventListener("click", () => {
     // 移除shell元素的close类
     shell.classList.remove("close");
-    mainbody.style.left = 260 + "px";
 })
 // 点击modeSwitch元素时触发事件
 modeSwitch.addEventListener("click", () => {
